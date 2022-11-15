482a17192026b71083a74d7b9dfb0a6ffa04fc5a
gab1144 <gabry.rinciari04@gmail.com>
2022-11-15 17:42:55 +0100

    [add] footer

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -1,12 +1,243 @@
 <script>
   export default {
-    name: 'AppFooter'
+    name: 'AppFooter',
+    data(){
+      return{
+        footerImg: 'footer.jpg',
+        links: [
+          {
+            linkListName: "Dc comics",
+            linkList: [
+              {
+                text: "Characters",
+                href: '#'
+              },
+              {
+                text: "Comics",
+                href: '#'
+              },
+              {
+                text: "Movies",
+                href: '#'
+              },
+              {
+                text: "TV",
+                href: '#'
+              },
+              {
+                text: "Games",
+                href: '#'
+              },
+              {
+                text: "Videos",
+                href: '#'
+              },
+              {
+                text: "News",
+                href: '#'
+              }
+            ]
+          },
+          {
+            linkListName: "Dc",
+            linkList: [
+              {
+                text: "Terms Of Use",
+                href: '#'
+              },
+              {
+                text: "Privacy policy(New)",
+                href: '#'
+              },
+              {
+                text: "Ad Choices",
+                href: '#'
+              },
+              {
+                text: "Advertising",
+                href: '#'
+              },
+              {
+                text: "Jobs",
+                href: '#'
+              },
+              {
+                text: "Subscriptions",
+                href: '#'
+              },
+              {
+                text: "Talent Workshops",
+                href: '#'
+              }
+              ,
+              {
+                text: "CPSC Certificates",
+                href: '#'
+              },
+              {
+                text: "Ratings",
+                href: '#'
+              },
+              {
+                text: "Shop Help",
+                href: '#'
+              },
+              {
+                text: "Contact Us",
+                href: '#'
+              }
+            ]
+          },
+          {
+            linkListName: "Sites",
+            linkList: [
+              {
+                text: "Dc",
+                href: '#'
+              },
+              {
+                text: "MAD Magazine",
+                href: '#'
+              },
+              {
+                text: "DC Kids",
+                href: '#'
+              },
+              {
+                text: "Dc Universe",
+                href: '#'
+              },
+              {
+                text: "Dc Power Visa",
+                href: '#'
+              }
+            ]
+          },
+          {
+            linkListName: "Shop",
+            linkList: [
+              {
+                text: "Shop Dc",
+                href: '#'
+              },
+              {
+                text: "Shop Dc Collectibles",
+                href: '#'
+              }
+            ]
+          }
+        ]
+      }
+    }
   }
 </script>
-
+  
 <template>
+  <footer>
+    <div class="footer-top">
+      <div class="container">
+        <div class="links"
+        v-for="list in links">
+          <h4>{{list.linkListName}}</h4>
+          <nav>
+            <ul>
+              <li 
+              v-for="link in list.linkList"><a :href="link.href">{{link.text}}</a></li>
+            </ul>
+          </nav>
+        </div>
+      </div>
+    </div>
+
+    <div class="footer-bottom">
+      <div class="container">
+
+        <div class="button">
+          <a href="#">Sign-up-now!</a>
+        </div>
+        
+        <div class="social-links">
+          <h4>Follow us</h4>
+          <ul>
+            <li>
+              <a href="#"><img src="../assets/img/footer-facebook.png" alt=""></a>
+            </li>
+            <li>
+              <a href="#"><img src="../assets/img/footer-twitter.png" alt=""></a>
+            </li>
+            <li>
+              <a href="#"><img src="../assets/img/footer-youtube.png" alt=""></a>
+            </li>
+            <li>
+              <a href="#"><img src="../assets/img/footer-pinterest.png" alt=""></a>
+            </li>
+            <li>
+              <a href="#"><img src="../assets/img/footer-periscope.png" alt=""></a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </footer>
 </template>
 
 
-<style scoped>
+<style lang="scss" scoped>
+  @use '../style/partials/variables' as *;
+  @use '../style/partials/mixin' as *;
+  .footer-top{
+    padding: 30px 0;
+    background-image: url("../assets/img/footer-bg.jpg");
+    color: white;
+    .container {
+    display: flex;
+    flex-wrap: wrap;
+    }
+    .links {
+      width: calc(100%/3);
+      h4{
+        text-transform: uppercase;
+      }
+      ul {
+        list-style: none;
+        a{
+          color: grey;
+          text-decoration: none;
+        }
+      }
+    }
+  }
+  .footer-bottom {
+    background-color: rgb(48, 48, 48);
+    color: white;
+    .container {
+      @include centerFlex('vertical');
+      justify-content: space-between;
+    }
+    .button {
+      border: 2px solid blue;
+      padding: 15px;
+      a {
+        color: white;
+        text-transform: uppercase;
+        text-decoration: none;
+      }
+    }
+    .social-links{
+      padding: 30px 0;
+      @include centerFlex('vertical');
+      h4 {
+        text-transform: uppercase;
+        margin-right: 20px;
+        color: blue;
+      }
+      ul{
+        display: flex;
+        list-style: none;
+        li {
+          margin: 0 5px;
+        }
+      }
+    }
+  }
 </style>
