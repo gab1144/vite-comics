e285505bfa3c85d49a51a02d52f7825a8b637ca2
gab1144 <gabry.rinciari04@gmail.com>
2022-11-16 15:12:41 +0100

    [add] tag e button

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -23,6 +23,8 @@
   <main>
     <Jumbotron/>
     <div class="container">
+      <span class="tag">Current series</span>
+
       <div class="card-area">
         <ComicCard
         v-for="(comic, index) of comics" 
@@ -30,6 +32,10 @@
         :cardImage="comic.thumb"
         :cardTitle="comic.series"/>
       </div>
+      
+      <button>
+        Load more
+      </button>
     </div>
 
     <BlueBar/>
@@ -39,13 +45,37 @@
 
 
 <style lang="scss" scoped>
+@use '../style/partials/variables' as *;
   main {
     background-color: black;
     color: white;
     .container {
       padding: 50px 0;
+      position: relative;
+      display: flex;
+      flex-direction: column;
+      .tag {
+        text-transform: uppercase;
+        position: absolute;
+        top: calc(-15px - 12.5px);
+        left: 0;
+        background-color: $primary-color;
+        padding: 15px 30px;
+        font-size: 25px;
+        font-weight: 800;
+      }
+      button {
+        background-color: $primary-color;
+        padding: 10px 60px;
+        text-transform: uppercase;
+        border: none;
+        color: white;
+        font-weight: 800;
+        font-size: 12px;
+        align-self: center;
+      }
     }
-
+    
     .card-area {
       display: flex;
       justify-content: space-between;
